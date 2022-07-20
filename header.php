f3ea988d69b4c549dcedeb8f5ad5086e98ffaef8
ashiqurrahmananik <anikewucse@gmail.com>
2022-07-21 01:32:20 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Stock</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="purchase.php">Purchase</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  href="sales.php">Sales</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  href="report.php">Report</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+    include "header.php";
+?>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div class="container">
+    <h6>Stock Status</h6>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Product Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Unit</th>
+      <th scope="col">Unit Price</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Nuts</td>
+      <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry</td>
+      <td>160</td>
+      <td>20</td>
+      <td>Update / Delete</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Milk</td>
+      <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry</td>
+      <td>250</td>
+      <td>85</td>
+      <td>Update / Delete</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Water</td>
+      <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry</td>
+      <td>250</td>
+      <td>30</td>
+      <td>Update / Delete</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
