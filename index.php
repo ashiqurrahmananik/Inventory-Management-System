849cf56c5cdd62c724701def97ae8b3588149fda
ashiqurrahmananik <anikewucse@gmail.com>
2022-08-29 01:05:19 +0600

    2nd commit

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
     include "header.php";
+    $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ims480";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM product";
+$result = mysqli_query($conn, $sql);
+
+
 ?>
 <html>
 <head>
@@ -7,11 +23,11 @@
 </head>
 <body>
     <div class="container">
-    <h6>Stock Status</h6>
+    <h5>Stock Status</h5>
     <table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
+      <!--<th scope="col">#</th>-->
       <th scope="col">Product Name</th>
       <th scope="col">Description</th>
       <th scope="col">Unit</th>
@@ -20,30 +36,28 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Nuts</td>
-      <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry</td>
-      <td>160</td>
-      <td>20</td>
-      <td>Update / Delete</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Milk</td>
-      <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry</td>
-      <td>250</td>
-      <td>85</td>
-      <td>Update / Delete</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Water</td>
-      <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry</td>
-      <td>250</td>
-      <td>30</td>
-      <td>Update / Delete</td>
-    </tr>
+   
+      <?php
+          if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+              ?>
+               <tr>
+                <!--<th scope="row"></th>-->
+                <td><?php echo $row["name"] ?></td>
+                <td><?php echo $row["des"] ?></td>
+                <td><?php echo $row["unit"] ?></td>
+                <td><?php echo $row["unit_price"] ?></td>
+                <td>Update / Delete</td>
+                </tr>
+                <?php }
+        } else {
+            echo "0 results";
+        }
+        ?>
+      
+
+    
   </tbody>
 </table>
 </div>
--- purchase.php
+++ purchase.php
@@ -1,3 +1,37 @@
 <?php
     include "header.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h5>Purchase</h5>
+    <form  action="purchase_action.php" method="post">
+  <div class="mb-3">
+    <label for="exampleInputName" class="form-label">Product Name</label>
+    <input type="text" name="name" class="form-control" id="exampleInputName">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputDes" class="form-label">Description</label>
+    <input type="text" name="des" class="form-control" id="exampleInputDes">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputUnit" class="form-label">Unit</label>
+    <input type="number" name="unit" class="form-control" id="exampleInputUnit">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputUnitprice" class="form-label">Unit Price</label>
+    <input type="number" name="unitprice" class="form-control" id="exampleInputUnitprice">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+</body>
+</html>
